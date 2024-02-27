eaff0a50942b2c34a08de726f911e61026b47611
atbucchi <alisson.t.bucchi@gmail.com>
2024-02-27 21:22:05 +0000

    tarefa 2

--- polls/routes.js
+++ polls/routes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const services = require("./services");
+const { createPollSchema } = require("./schemas");
 
 router.get("/", async (req, res) => {
   const polls = await services.getAllPolls();
@@ -18,6 +19,29 @@ router.get("/:id", async (req, res) => {
   }
   res.status(200).json(poll); 
 }); 
+
+//#2 Implementar um endpoint que permita criar uma poll. Uma poll é constituida por uma pergunta e uma lista de opções.
+router.post("/", async (req, res) => {
+  const { error, value } = createPollSchema.validate(req.body);
+  
+  if (error) {
+    return res.status(400).json({erro: "Poll not create!" }); 
+  }
+
+  try {
+    const insertResult = await services.getAllPolls().insertOne(value); 
+    const result = await services.getPollById(insertResult.insertedId); 
+    res.status(201).json({ message: "poll created successFully!", result }); 
+
+  }
+  catch(error) {
+    res.status(500).json({message: "poll not created!"}); 
+  }
+}); 
+
+//#3 Implementar um endpoint que permita votar numa opção de uma poll.
+
+router.put("/:id", async (req, res) => {
 
 
 
